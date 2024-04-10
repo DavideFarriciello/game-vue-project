9de1481215eb778bd155c711d23412afb5341d55
DavideFarriciello <davide.farriciello@gmail.com>
2024-04-10 08:24:18 +0100

    add

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,26 @@
 <template>
-  <h1>{{ title }}</h1>
-  <p>Welcome...</p>
-  <teleport to="#modals" v-if="showModal">
-    <Modal theme="sale" @close="toggleModal">
-      <template v-slot:links>
-        <a href="#">sign up</a>
-        <a href="#">more info</a>
-      </template>
-      <h1>nija</h1>
-      <p>take thi shit</p>
-    </Modal>
-  </teleport>
-  <div v-if="showModalTwo">
-    <teleport to="#modals" theme="sale" @close="toggleModalTwo">
-      <template v-slot:links>
-        <a href="#">disc</a>
-        <a href="#">shit</a>
-      </template>
-      <h1>aglia</h1>
-      <p>nn farlo</p>
-    </teleport>
-  </div>
-  <button @click="toggleModal">Open Modal</button>
-  <button @click="toggleModalTwo">Open Second Modal</button>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: { Modal },
-  data() {
-    return {
-      title: 'my first vue app',
-      showModal: false,
-      showModalTwo: false,
-    }
-  },
-  methods: {
-    toggleModal() {
-      this.showModal = !this.showModal
-    },
-    toggleModalTwo() {
-      this.showModalTwo = !this.showModalTwo
-    }
+  components: {
+    HelloWorld
   }
 }
 </script>
 
 <style>
-#app,
-#modals {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-h1 {
-  border-bottom: 1px solid #ddd;
-  display: inline-block;
-  padding-bottom: 10px;
 }
 </style>
