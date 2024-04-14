d0863cbf1f5911dbbebab51387d1c8a59d578fbf
DavideFarriciello <davide.farriciello@gmail.com>
2024-04-14 21:03:16 +0100

    init

---
+++ game-vue-project/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="nav">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
