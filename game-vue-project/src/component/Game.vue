6383e1182e10c30df02a0966061d454f9215a28b
DavideFarriciello <davide.farriciello@gmail.com>
2024-04-16 19:09:20 +0100

    add

--- game-vue-project/src/component/Game.vue
+++ game-vue-project/src/component/Game.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-wrap justify-center p-5">
-    <div v-for="game in games" :key="game" @click="onSubmit"
+    <div v-for="game in games" :key="game"
       class="bg-white-game rounded-lg w-64 ml-7 mb-20 shadow-2xl hover-shadow-red transition-all duration-300 ease-in-out hover:scale-105 hover:cursor-pointer">
       <img :src="game.image" :alt="game.name" class="pt-1 px-1">
       <h2 class="text-xl text-center my-4">{{ game.name }}</h2>
