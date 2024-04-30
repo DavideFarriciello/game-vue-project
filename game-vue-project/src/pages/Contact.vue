cb490fd6ef1356198a232f55092ebb1ff30a100d
DavideFarriciello <davide.farriciello@gmail.com>
2024-04-30 16:43:50 +0100

    size fixed

--- game-vue-project/src/pages/Contact.vue
+++ game-vue-project/src/pages/Contact.vue
@@ -1,27 +1,27 @@
 <template>
   <div>
     <h1
-      class="mt-16 text-7xl flex justify-center pb-7 font-bold text-gradient-from-fucsia hover:-translate-y-1 transition duration-300 ease-in-out">
+      class="lg:mt-16 xs:mt-8 lg:text-7xl xs:text-5xl flex justify-center pb-7 font-bold text-gradient-from-fucsia hover:-translate-y-1 transition duration-300 ease-in-out">
       GET IN TOUCH</h1>
-    <div class="flex justify-between mx-60 my-10 ">
+    <div class="flex justify-between lg:mx-60 xs:mx-2 lg:my-10 xs:my-4 ">
       <a href="https://www.linkedin.com/in/davide-farriciello" target="_blank"
-        class="pi pi-linkedin text-7xl text-blue-600 hover:-translate-y-2 transition duration-300 ease-in-out"></a>
+        class="pi pi-linkedin lg:text-7xl xs:text-5xl text-blue-600 hover:-translate-y-2 transition duration-300 ease-in-out"></a>
       <a href="https://www.instagram.com/davide_farriciello" target="_blank"
-        class="pi pi-instagram text-7xl text-pink-600 hover:-translate-y-2 transition duration-300 ease-in-out"></a>
+        class="pi pi-instagram lg:text-7xl xs:text-5xl text-pink-600 hover:-translate-y-2 transition duration-300 ease-in-out"></a>
       <a href="https://wa.me/353830044870" target="_blank"
-        class="pi pi-whatsapp text-7xl text-green-500 hover:-translate-y-2 transition duration-300 ease-in-out"></a>
+        class="pi pi-whatsapp lg:text-7xl xs:text-5xl text-green-500 hover:-translate-y-2 transition duration-300 ease-in-out"></a>
       <div class="flex flex-row">
         <a href="mailto:davide.farriciello@gmail.com" target="_blank"
-          class="pi pi-envelope text-7xl text-red-700 hover:-translate-y-2 transition duration-300 ease-in-out"></a>
+          class="pi pi-envelope lg:text-7xl xs:text-5xl text-red-700 hover:-translate-y-2 transition duration-300 ease-in-out"></a>
       </div>
       <div class="flex flex-row">
         <a href="https://www.davidefarriciello.dev" target="_blank"
-          class="pi pi-desktop text-7xl text-fuchsia-800 hover:-translate-y-2 transition duration-300 ease-in-out "></a>
+          class="pi pi-desktop lg:text-7xl xs:text-5xl text-fuchsia-800 hover:-translate-y-2 transition duration-300 ease-in-out "></a>
       </div>
     </div>
     <div class="flex flex-wrap justify-center p-5">
       <div
-        class="bg-white-game rounded-lg w-[40%] h-[390px] my-8 shadow-2xl transition-all duration-300 ease-in-out hover:scale-105 hover-shadow-red">
+        class="bg-white-game rounded-lg lg:w-[40%] xs:w-[90%] lg:h-[390px] xs:[370px] lg:my-8 xs:my-1 shadow-2xl transition-all duration-300 ease-in-out hover:scale-105 hover-shadow-red">
 
         <form class="flex flex-col px-4" @submit="handleSubmit">
           <input type="text" name="name" placeholder="Name" class="mt-3 mb-3 p-4 rounded-lg outline-none">
