ed5cb2eb81b5b45e69a44db0d47842dcc10ecdf7
DavideFarriciello <davide.farriciello@gmail.com>
2024-05-25 00:37:16 +0100

    add

--- game-vue-project/src/component/Sale.vue
+++ game-vue-project/src/component/Sale.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-  <div class="flex justify-center  bg-gradient-to-r from-slate-950 to-fuchsia-900 transition-all duration-300 ease-in-out hover:scale-105">
-    <h1 class="lg:text-3xl my-4 xs:text-xl text-white">Free shipping if you spend over then 50€</h1>
+    <div class="flex justify-center bg-gradient-to-r from-slate-950 to-fuchsia-900 transition-all duration-300 ease-in-out hover:scale-105">
+      <h1 class="lg:text-3xl my-4 xs:text-xl text-white">Free shipping if you spend over then 50€</h1>
+    </div>
+    <div class="relative w-[100%] h-80 overflow-hidden mt-0 cursor-pointer">
+      <button class="absolute top-52 left-32  z-10 bg-slate-950 text-white font-bold lg:text-xl lg:w-52 xs:w-28 xs:text-sm  xs:py-2 lg:py-3 ml-2 mt-5 mb-2 rounded-3xl shadow hover:bg-fuchsia-900 hover:shadow-lg hover:-translate-y-1 transition duration-300 ease-in-out">Buy Now</button>
+      <video src="/gallery/minecraft-banner.mp4" autoplay muted loop style="width: 100%; height: 100%; object-fit:cover;"></video>
+    </div>
   </div>
-  <div class="w-[100%] h-80 overflow-hidden mt-0 cursor-pointer">
-    <video src="/gallery/minecraft-banner.mp4" autoplay muted loop style="width: 100%; height: 100%; object-fit:cover;"></video>
-  </div>
-</div>
 </template>
 
 <script setup>
